a150ce345cad3d0da774026e107ad46b5c4dad90
Viktor Todosijevic <todosijevicviktor998@gmail.com>
2021-03-03 19:05:42 +0100

    PV works

--- Q-Learning-MPPT/Buck-converter/main.m
+++ Q-Learning-MPPT/Buck-converter/main.m
@@ -3,14 +3,14 @@ close all;
 clear variables;
 %%
 sample_time = 1e-5; % sec
-sim_duration = 0.1; % sec
+sim_duration = 1; % sec
 %%
 Irradiance = 1000; % W/m^2
 Temperature = 25; % 'C
 %%
 Vout = 6; % V
 Vin = 17.6; % V
-fsw = 1e3; % Hz
+fsw = 1e4; % Hz
 Iout_max = 1.84; % A
 delta_IL = 0.2 * Iout_max; % A
 delta_Vout = 0.01 * Vout; % V
@@ -19,7 +19,7 @@ L = Vout * (Vin - Vout) / (delta_IL * fsw * Vin); % H
 
 Cout = delta_IL / (8 * fsw * delta_Vout); % F
 
-Cin = 1e-6; % F
+Cin = 300e-6; % F
 
 r_small = 1e-3; % Ohm
 %%
